edaaf0468848b091a2b7a3608214a1ccf61013c9
Nitish Kumar <56731812+Nitish2940@users.noreply.github.com>
2020-08-16 23:14:59 +0530

    Add files via upload

---
+++ experiment/simulator/js/functions.js
@@ -0,0 +1,21 @@
+function fun0()
+{
+    document.getElementById('w0').style.display='none';
+    document.getElementById('w1').style.display='block';
+}
+function fun1()
+{
+    document.getElementById('w1').style.display='none';
+    document.getElementById('w2').style.display='block';
+
+}
+function fun2()
+{
+    document.getElementById('w2').style.display='none';
+    document.getElementById('w3').style.display='block';
+}
+function fun3()
+{
+    document.getElementById('w3').style.display='none';
+    document.getElementById('w4').style.display='block';
+}
